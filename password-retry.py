0f2618ec4cc0526d46dee125e2ecc57ff26489cd
Mike LEUNG <ckleungah@gmail.com>
2020-02-12 11:40:45 +0800

    second version done

--- password-retry.py
+++ password-retry.py
@@ -1,10 +1,14 @@
 password = 'a123456'
 i = 3 # remaining chances
 while i > 0:
+	i = i - 1
 	pwd = input('please input your password: ')
 	if pwd == password:
 		print('login successful!')
 		break #break the loop
 	else: 
-		i = i - 1
-		print('password incorrect! you still have', i, 'chances')
+		print('password incorrect!')
+		if i > 0:
+			print('you still have', i, 'chances')
+		else:
+			print('account has been locked!')
